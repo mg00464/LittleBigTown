9432e454802bd7de5cd49ccce6c1d574b73e71f0
mg00464 <mg00464@surrey.ac.uk>
2018-11-03 19:46:25 +0000

    Added images, bootstrap element on homepage, routes to home and contct page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,8 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get home_home_url
     assert_response :success
-  end
 
+    assert_select 'title', 'Little Big Town'
+    assert_select 'h1', 'welcome to my web app'
+  end
 end
